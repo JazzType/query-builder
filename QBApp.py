8c9e8a1bdc40909ee284b10808278732642acfa7
Aditya Bharadwaj <aditya21nov@gmail.com>
2017-03-05 16:19:18 +0530

    Video position is passed as binary search param

--- QBApp.py
+++ QBApp.py
@@ -1,4 +1,5 @@
 import kivy
+import numpy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
@@ -7,10 +8,14 @@ from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import ScreenManager, Screen, SwapTransition
 from neo4j.v1 import GraphDatabase, basic_auth
 
+driver = GraphDatabase.driver("bolt://localhost:7687", auth = basic_auth("neo4j", "aditya"))
+session = driver.session()
+
 class QueryBuilder(BoxLayout):
 	qb_button = ObjectProperty()
 	screen_manager = ObjectProperty()
 	toggleColumn = False
+	video_player = ObjectProperty()
 	def update_button(self, qb_button, main_layout):
 		if not self.toggleColumn:
 			self.toggleColumn = True
@@ -18,14 +23,16 @@ class QueryBuilder(BoxLayout):
 			for i in range(3):
 				column3.add_widget(Label(text='column3item{}'.format(i)))
 			main_layout.add_widget(column3)
-
-	def change_screen(self, screen_name):
+		Neo4jInterface().init(self.video_player.position)
+	def change_screen(self, screen_name):		
 		self.screen_manager.current = screen_name
 			
+
+	
+		
 class QBApp(App):
 	def build(self):
 		return QueryBuilder()		
-
 
 class Neo4jInterface():
 	def binary_search(self, tuple_array, pause_time):
@@ -49,18 +56,13 @@ class Neo4jInterface():
 		print("Not in any range, returning", tuple_array[mid-1])
 		return mid-1
 
-	def init(self):
-		driver = GraphDatabase.driver("bolt://localhost:7687", auth = basic_auth("neo4j", "password"))
-		session = driver.session()
+	def init(self, pause_time):
 		end_times = session.run("MATCH (n) WHERE EXISTS(n.end_time) RETURN DISTINCT 'node' as element, n.end_time AS end_time UNION ALL MATCH ()-[r]-() WHERE EXISTS(r.end_time) RETURN DISTINCT 'relationship' AS element, r.end_time AS end_time")
 		end_times = [float(record['end_time']) for record in end_times]
 		start_times = session.run("MATCH (n) WHERE EXISTS(n.start_time) RETURN DISTINCT 'node' as element, n.start_time AS start_time UNION ALL MATCH ()-[r]-() WHERE EXISTS(r.start_time) RETURN DISTINCT 'relationship' AS element, r.start_time AS start_time")
 		start_times = [float(record['start_time']) for record in start_times]
-		session.close()	
 		tuple_array = [(start_time, end_time) for start_time, end_time in zip(start_times, end_times)]				
-		print(self.binary_search(tuple_array, 12110.0))
+		print(self.binary_search(tuple_array, pause_time))
 
 if __name__ == '__main__':
-	#QBApp().run()
-	Neo4jInterface().init()
-
+	QBApp().run()
