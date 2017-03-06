4dd239e9ce37c46aa68b3e761a26e7046f81d2f3
Aditya Bharadwaj <aditya21nov@gmail.com>
2017-03-06 22:25:35 +0530

    Added slightly more complex queries

--- QBApp.py
+++ QBApp.py
@@ -1,4 +1,5 @@
 import kivy
+import time
 import numpy
 from kivy.app import App
 from kivy.lang import Builder
@@ -16,8 +17,8 @@ class QueryBuilder(BoxLayout):
 	video_player = ObjectProperty()
 	player_column = BoxLayout(orientation='vertical', size_hint=(0.1, 1))
 	res_layout = ObjectProperty()
-	print type(video_player)
-	print type(res_layout)
+	
+
 	def update_button(self, main_layout, res_layout):
 		Neo4jInterface().init(self.video_player.position, main_layout, res_layout)
 
@@ -33,10 +34,19 @@ class QueryBuilder(BoxLayout):
 			main_layout.add_widget(self.player_column)			
 			self.toggleColumn = True
 		
-	def insert_data(self, player_type, player_list, res_layout):
-		res_layout.add_widget(Label(text = "Best " + player_type + " for RCB : " + player_list[0]))
-		res_layout.add_widget(Label(text = "Best " + player_type +" for CSK : " + player_list[1]))
+	def insert_player_data(self, player_type, player_list, res_layout):
+		stats_column = BoxLayout(orientation = "vertical")
+		stats_column.add_widget(Label(text = player_type + " of the Match"))
+		stats_column.add_widget(Label(text = "Best " + player_type + " for RCB : " + player_list[0]))
+		stats_column.add_widget(Label(text = "Best " + player_type +" for CSK : " + player_list[1]))
+		res_layout.add_widget(stats_column)
 		
+	def insert_info(self, time_tuple, players, ball, res_layout):
+		info_column = BoxLayout(orientation = "vertical")
+		info_column.add_widget(Label(text = "Over : " + ball))
+		info_column.add_widget(Label(text = "Batsman : " + players[0] + " - Bowler : " + players[1]))
+		info_column.add_widget(Label(text = "Event Timestamps : "  + str(time_tuple[0]) + " - " + str(time_tuple[1]) ))
+		res_layout.add_widget(info_column)
 		
 class QBApp(App):
 	def build(self):
@@ -69,29 +79,37 @@ class Neo4jInterface():
 
 	def populate_objects(self, time_tuple, main_layout):
 		ball = self.session.run("MATCH (n {start_time: '" + str(time_tuple[0]) + "', end_time:'" + str(time_tuple[1]) + "'}) RETURN DISTINCT 'node' as element, n.over as over, n.innings as innings, n.batsman as batsman, n.bowler as bowler UNION ALL MATCH ()-[r]-() WHERE EXISTS(r.end_time) RETURN DISTINCT 'relationship' AS element, r.over as over, r.innings as innings, r.batsman as batsman, r.bowler as bowler")
-		
 		self.QueryBuilderObject.insert_players([(str(record['batsman']), str(record['bowler'])) for record in ball], main_layout)
 			
 	def get_best_batsmen(self, time_tuple, res_layout):
 		batsmen = self.session.run('MATCH (n) WHERE EXISTS(n.BestBatsman) RETURN DISTINCT "node" as element, n.BestBatsman AS BestBatsman LIMIT 25 UNION ALL MATCH ()-[r]-() WHERE EXISTS(r.BestBatsman) RETURN DISTINCT "relationship" AS element, r.BestBatsman AS BestBatsman LIMIT 25')
-		
-		self.QueryBuilderObject.insert_data('Batsman', [str(record['BestBatsman']) for record in batsmen], res_layout)
-
+	
+		self.QueryBuilderObject.insert_player_data('Batsmen', [str(record['BestBatsman']) for record in batsmen], res_layout)
 			
 	def get_best_bowlers(self, time_tuple, res_layout):
 		bowlers = self.session.run('MATCH (n) WHERE EXISTS(n.BestBowler) RETURN DISTINCT "node" as element, n.BestBowler AS BestBowler LIMIT 25 UNION ALL MATCH ()-[r]-() WHERE EXISTS(r.BestBowler) RETURN DISTINCT "relationship" AS element, r.BestBowler AS BestBowler LIMIT 25')
-		
-		self.QueryBuilderObject.insert_data('Bowler',[str(record['BestBowler']) for record in bowlers], res_layout)
 			
+		self.QueryBuilderObject.insert_player_data('Bowlers',[str(record['BestBowler']) for record in bowlers], res_layout)
+	
+	def get_event_details(self, time_tuple, res_layout):
+		query = self.session.run("MATCH (b:Ball) WHERE b.start_time = '"+ str(time_tuple[0]) + "' AND b.end_time = '" + str(time_tuple[1]) + "' RETURN b.batsman as batsman, b.bowler as bowler ,b.runs as runs ,b.over as over, b.ball as ball")
+		event = query.single()
+		players = [event['batsman'], event['bowler']]
+		ball = str(event['over']) + "." + str(event['ball'])
+		
+		self.QueryBuilderObject.insert_info(time_tuple, players, ball , res_layout)
+	
 	def init(self, pause_time, main_layout, res_layout):
 		end_times = self.session.run("MATCH (n) WHERE EXISTS(n.end_time) RETURN DISTINCT 'node' as element, n.end_time AS end_time UNION ALL MATCH ()-[r]-() WHERE EXISTS(r.end_time) RETURN DISTINCT 'relationship' AS element, r.end_time AS end_time")
 		end_times = [float(record['end_time']) for record in end_times]
 		start_times = self.session.run("MATCH (n) WHERE EXISTS(n.start_time) RETURN DISTINCT 'node' as element, n.start_time AS start_time UNION ALL MATCH ()-[r]-() WHERE EXISTS(r.start_time) RETURN DISTINCT 'relationship' AS element, r.start_time AS start_time")
 		start_times = [float(record['start_time']) for record in start_times]
-		tuple_array = [(start_time, end_time) for start_time, end_time in zip(start_times, end_times)]				
-		self.populate_objects(tuple_array[self.binary_search(tuple_array, pause_time)], main_layout)
-		self.get_best_batsmen(tuple_array[self.binary_search(tuple_array, pause_time)], res_layout)
-		self.get_best_bowlers(tuple_array[self.binary_search(tuple_array, pause_time)], res_layout)
+		tuple_array = [(start_time, end_time) for start_time, end_time in zip(start_times, end_times)]	
+		time_tuple = tuple_array[self.binary_search(tuple_array, pause_time)]
+		self.populate_objects(time_tuple, main_layout)
+		self.get_event_details(time_tuple, res_layout)
+		self.get_best_batsmen(time_tuple, res_layout)
+		self.get_best_bowlers(time_tuple, res_layout)
 		
 if __name__ == '__main__':
 	QBApp().run()
