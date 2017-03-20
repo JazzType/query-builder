3475901cd30db4ac997631ff5ce8730067fd9e40
Aditya Bharadwaj <aditya21nov@gmail.com>
2017-03-20 23:03:52 +0530

    Added Current over highlights, PvP highlight seek
    Need to fix Theme a bit again

--- QBApp.py
+++ QBApp.py
@@ -14,7 +14,6 @@ from kivy.graphics.instructions import Canvas
 from kivy.graphics.context_instructions import Color
 from kivy.graphics import Rectangle
 
-
 class QueryBuilder(BoxLayout):
 	driver = GraphDatabase.driver("bolt://localhost:7687", auth = basic_auth("neo4j", "password"))
 	session = driver.session()	
@@ -24,10 +23,10 @@ class QueryBuilder(BoxLayout):
 	toggleColumn = False
 	toggleJumps = False
 	video_player = ObjectProperty()
-	player_column = ObjectProperty()
+	player_column = BoxLayout()
 	times_column = BoxLayout(orientation = 'vertical',size_hint=(0.1, 0.65))
-	res_layout = ObjectProperty()	
-	
+	res_layout = ObjectProperty()
+
 
 	def binary_search(self, tuple_array, pause_time):
 		low = 0
@@ -51,16 +50,21 @@ class QueryBuilder(BoxLayout):
 		return mid-1
 
 	def populate_objects(self, time_tuple):
-		ball = self.session.run("MATCH (b:Ball) WHERE b.start_time = '"+ str(time_tuple[0]) + "' AND b.end_time = '" + str(time_tuple[1]) + "' RETURN b.batsman as batsman, b.bowler as bowler ,b.runs as runs ,b.over as over, b.ball as ball")
+		ball = self.session.run("MATCH (b:Ball) WHERE b.start_time = '"+ str(time_tuple[0]) + "' AND b.end_time = '" + str(time_tuple[1]) + "' RETURN b.batsman as batsman, b.bowler as bowler ,b.runs as runs ,b.over as over, b.ball as ball, b.innings as innings")
 		event = ball.single()
 		players = [event['batsman'], event['bowler']]
 		ball = str(event['over']) + "." + str(event['ball'])
 		runs = str(event['runs'])
-		self.insert_players(players,ball,runs)
+		over = str(event['over'])
+		inning = str(event['innings'])
+		overs_list = self.session.run("MATCH (b:Ball) WHERE b.over = '" + over + "' and b.innings = '" + inning + "' return b.runs" )
+		runs_list = [record['b.runs'].encode('UTF-8') for record in overs_list]
+		query = self.session.run("MATCH (b:Ball) WHERE b.over = '" + over + "' and b.innings = '" + inning + "' return b.start_time as start_time")
+		ball_times = [str(record['start_time']) for record in query]
+		self.insert_players(players,ball,runs,over, runs_list, ball_times)
 			
 	def get_best_batsmen(self, time_tuple):
 		batsmen = self.session.run('MATCH (n) WHERE EXISTS(n.BestBatsman) RETURN DISTINCT "node" as element, n.BestBatsman AS BestBatsman LIMIT 25 UNION ALL MATCH ()-[r]-() WHERE EXISTS(r.BestBatsman) RETURN DISTINCT "relationship" AS element, r.BestBatsman AS BestBatsman LIMIT 25')
-	
 		self.insert_player_data('Batsmen', [str(record['BestBatsman']) for record in batsmen])
 			
 	def get_best_bowlers(self, time_tuple):
@@ -68,20 +72,37 @@ class QueryBuilder(BoxLayout):
 			
 		self.insert_player_data('Bowlers',[str(record['BestBowler']) for record in bowlers])
 	
-	def get_event_details(self, time_tuple):
-		query = self.session.run("MATCH (b:Ball) WHERE b.start_time = '"+ str(time_tuple[0]) + "' AND b.end_time = '" + str(time_tuple[1]) + "' RETURN b.batsman as batsman, b.bowler as bowler ,b.runs as runs ,b.over as over, b.ball as ball")
+	'''def get_event_details(self, time_tuple):
+		query = self.session.run("MATCH (b:Ball) WHERE b.start_time = '"+ str(time_tuple[0]) + "' AND b.end_time = '" + str(time_tuple[1]) + "' RETURN b.batsman as batsman, b.bowler as bowler ,b.runs as runs ,b.over as over, b.ball as ball, b.innings as innings")
 		event = query.single()
 		players = [event['batsman'], event['bowler']]
 		ball = str(event['over']) + "." + str(event['ball'])
 		self.insert_info(time_tuple, players, ball)
 		self.load_clips(players)
-	
-	def load_clips(self, players):
-		query = self.session.run("MATCH (b:Ball) WHERE b.batsman = '" + players[0] +  "' AND b.bowler = '" + players[1] + "' AND b.runs = '4' return b.start_time as start_time")
-		good_times = [str(record['start_time']) for record in query]
-		self.insert_clips(good_times)
+	'''
+	def get_event_details(self, time_tuple):
+		query = self.session.run("MATCH (b:Ball) WHERE b.start_time = '"+ str(time_tuple[0]) + "' AND b.end_time = '" + str(time_tuple[1]) + "' RETURN b.batsman as batsman, b.bowler as bowler ,b.runs as runs ,b.over as over, b.ball as ball, b.innings as innings")
+		event = query.single()
+		players = [event['batsman'], event['bowler']]
+		ball = str(event['over']) + "." + str(event['ball'])
+		over = str(event['over'])
+		inning = str(event['innings'])
+		print over,inning
+		self.insert_info(time_tuple, players, ball)
+		self.load_clips(players)
 		
-	def init(self, pause_time):
+	def load_clips(self, players):
+		query = self.session.run("MATCH (b:Ball) WHERE b.batsman = '" + players[0] +  "' AND b.bowler = '" + players[1] + "' AND (b.runs = '4' or b.runs = '6' or b.runs = 'OUT') return b.start_time as start_time, b.runs as runs")
+		#times = [str(record['start_time']) for record in query]
+		#runs = [str(record['runs']) for record in query]
+		times = []
+		runs = []
+		for record in query:
+			times.append(str(record['start_time']))
+			runs.append(str(record['runs']))
+		self.insert_clips(players, runs, times)
+		
+	def init(self, pause_time): 
 		self.res_layout.clear_widgets()
 		end_times = self.session.run("MATCH (n) WHERE EXISTS(n.end_time) RETURN DISTINCT 'node' as element, n.end_time AS end_time UNION ALL MATCH ()-[r]-() WHERE EXISTS(r.end_time) RETURN DISTINCT 'relationship' AS element, r.end_time AS end_time")
 		end_times = [float(record['end_time']) for record in end_times]
@@ -101,15 +122,24 @@ class QueryBuilder(BoxLayout):
 	def change_screen(self, screen_name):		
 		self.screen_manager.current = screen_name
 		
-	def insert_players(self, player_list, ball, runs):		
+	def insert_players(self, player_list, ball, runs, over, runs_list, ball_times):		
 		#if type(self.player_column) is BoxLayout:
-		self.player_column.clear_widgets()						
+		self.player_column.clear_widgets()	
+		over_layout = BoxLayout(orientation='horizontal')		
+		x = 0
+		for i in ball_times:
+			button = Button(text = str(runs_list[x]), size_hint =(0.75,0.15))
+			over_layout.add_widget(button)
+			button.bind(on_press = lambda x,i=i : self.play_to_pos(i))
+			x = x + 1
 		self.player_column.add_widget(Label(text='Batsman: ' + str(player_list[0]), color=(0, 0, 0, 1)))
 		self.player_column.add_widget(Label(text='Bowler: ' + str(player_list[1]), color=(0, 0, 0, 1)))
 		self.player_column.add_widget(Label(text='Over: ' + ball, color=(0, 0, 0, 1)))
 		self.player_column.add_widget(Label(text='Runs: ' + runs, color=(0, 0, 0, 1)))
+		self.player_column.add_widget(Label(text = "This Over:", color=(0, 0, 0, 1)))
+		self.player_column.add_widget(over_layout)
 		#if not self.toggleColumn:
-		#	self.main_layout.add_widget(self.player_column, len(self.main_layout.children))			
+		#	self.main_layout.add_widget(self.player_column)				
 		#	self.toggleColumn = True
 		
 	def insert_player_data(self, player_type, player_list):
@@ -117,7 +147,7 @@ class QueryBuilder(BoxLayout):
 		stats_column.add_widget(Label(text = player_type + " of the Match", color=(0, 0, 0, 1)))
 		stats_column.add_widget(Label(text = "Best " + player_type + " for RCB : " + player_list[0], color=(0, 0, 0, 1)))
 		stats_column.add_widget(Label(text = "Best " + player_type +" for CSK : " + player_list[1], color=(0, 0, 0, 1)))
-		self.res_layout.add_widget(widget=stats_column)
+		self.res_layout.add_widget(stats_column)
 		
 	def insert_info(self, time_tuple, players, ball):
 		info_column = BoxLayout(orientation = "vertical")
@@ -129,15 +159,19 @@ class QueryBuilder(BoxLayout):
 		info_column.add_widget(backBtn)
 		self.res_layout.add_widget(info_column)
 			
-	def insert_clips(self, times):
-		print(times)
+	def insert_clips(self, players, runs, times):
+		print runs
 		if type(self.times_column) is BoxLayout:
 			self.times_column.clear_widgets()
-		y_hint = 1/len(times)
+		self.times_column.add_widget(Label(text = players[0] + " vs " + players[1]))
+		x = 0
+		if not len(times):
+			self.times_column.add_widget(Label(text = "No Highlights Found"))
 		for i in times:
-			button = Button(text = "Jump to : " + i, size_hint =(1,y_hint))
+			button = Button(text = str(runs[x]), size_hint =(1,1))
 			self.times_column.add_widget(button)
-			button.bind(on_press = lambda x,i=i : self.play_to_pos(i))
+			button.bind(on_press = lambda x,i=i : self.play_to_pos(i))	
+			x = x + 1
 		if not self.toggleJumps:
 			self.main_layout.add_widget(self.times_column)			
 			self.toggleJumps = True
